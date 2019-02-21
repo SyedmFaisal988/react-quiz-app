4b20fd09af5aabb38615e2503cf558817869d42d
syed muhammad faisal <syed.mfaisal988@gmail.com>
2019-02-21 19:44:58 +0500

    media query completed, i think stable version

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Router from './Route';
 
-const data = {
-  userName: "admin",
-  totQuest: "2",
-  loadTest: [0,0],
-  testName:"Blah blah",
-  correctAns: 2,
-}
-
 class App extends Component {
   render() {
     return (
